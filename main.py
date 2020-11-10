28c87d9675334cbc591e58c14616dce5a2159910
tanishq1010 <73070362+tanishq1010@users.noreply.github.com>
2020-11-10 16:36:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://10.141.12.55:27017/")
+database = client["learning_interventions"]
+collection = database["question_count"]
+
+# Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+
+query = {}
+query["lm_level"] = "chapter"
+
+projection = {}
+projection["_id"] = 0
+projection["questions.normal_questions"] = 1.0
+projection["lm_level_code"] = 1.0
+projection["concept_id"] = 1.0
+
+cursor = collection.find(query, projection=projection)
+df = pd.DataFrame(columns=['NQE_question_id'])
+df.to_csv('NQE_data.csv',index=False)
+i=0
+for doc in cursor:
+    df = pd.read_csv("NQE_data.csv")
+    # print(doc)
+    LIST = doc['questions']['normal_questions']
+    # print(LIST)
+    for question in LIST:
+        df.loc[len(df)] = [question]
+
+    df.to_csv('NQE_data.csv', index=False)
+    i+=1
+    print(i)
+    # print(df)
+
+# df = pd.DataFrame(list(cursor))
+# print(df)
+
+# print(get_question_meta_tag_CG('EM0000001-en',23))
