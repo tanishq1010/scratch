12d05d61c78fb019b7abe54d814c14cd5d476465
tanishq1010 <73070362+tanishq1010@users.noreply.github.com>
2021-02-06 14:27:46 +0530

    Create main22.py

---
+++ main22.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pymongo import MongoClient
+import numpy as np
+import traceback
+
+client = MongoClient("mongodb://10.141.12.55:27017/")
+database = client["learning_interventions"]
+collection = database["question_count"]
+
+# Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+
+query = {}
+
+projection = {}
+projection["_id"] = 0
+projection["questions.normal_questions"] = 1.0
+
+cursor = collection.find(query, projection=projection).limit(100)
+
+LIST_app = []
+for doc in cursor:
+    try:
+        LIST = doc['questions']['normal_questions']
+        LIST_app = LIST_app + LIST
+    except:
+        print(traceback.format_exc())
+
+df = pd.DataFrame(data=LIST_app, columns=['id'])
+
+
+df.to_csv('mongo_data.csv', index=False)
