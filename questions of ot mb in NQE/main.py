63b92958b9af9c21fcc07f02e481febab95ac5b3
tanishq1010 <73070362+tanishq1010@users.noreply.github.com>
2021-02-08 17:14:24 +0530

    Add files via upload

---
+++ questions of ot mb in NQE/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('testing.csv')
+df1 = pd.read_csv('OT and Mockbank Qn ids (1).csv')
+df1.insert(2, "Present in NQE in published state", '')
+for ind in df1.index:
+    # print(df1['question_id'][ind])
+    df_new = df.loc[df['NQE_question_id'] == df1['question_id'][ind]]
+
+    if len(df_new) > 0:
+        df1['Present in NQE in published state'][ind] = 'Yes'
+    else:
+        df1['Present in NQE in published state'][ind] = 'No'
+
+df1.to_csv('OT_MB_questions_status_in_NQE.csv', index=False)
